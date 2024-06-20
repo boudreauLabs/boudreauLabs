7788779d1a61fe1cc3e8bb3629200d58181518dc
boudreauLabs <boudreaulabs@gmail.com>
2024-06-20 13:43:03 -0400

    bump gulp version up

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,11 @@
 "use strict";
 
-const { watch, src, dest, parallel } = require('gulp');
-const autoprefixer = require('autoprefixer');
+const { watch, src, dest, series, parallel } = require('gulp');
 const browsersync = require('browser-sync').create();
 const concat = require('gulp-concat');
-const cleanCss = require('gulp-clean-css');
 const del = require('del');
-const gulp = require('gulp');
 const gulpIf = require('gulp-if');
 const rename = require('gulp-rename');
-const sass = require('gulp-sass')(require('sass'));
-const sassGlob = require('gulp-sass-glob');
-const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
 
 const cfg = require( './gulp-config.json' );
@@ -19,22 +13,10 @@ const paths = cfg.paths;
 
 const isProduction = process.argv.includes('--production');
 
-function scssStyles() {
-  return src(paths.styles.in, { allowEmpty: true })
-    .pipe(sassGlob())
-    .pipe(gulpIf(!isProduction, sourcemaps.init()))
-    .pipe(sass({ outputStyle: 'expanded'})).on('error', sass.logError)
-    .pipe(gulpIf(isProduction, cleanCss()))
-    .pipe(gulpIf(isProduction, rename({ suffix: '.min' })))
-    .pipe(gulpIf(!isProduction, sourcemaps.write('.'))) // Writes sourcemaps only in development
-    .pipe(dest(paths.styles.out))
-    .pipe(browsersync.stream());
-}
-
 function themeScript() {
-  return gulp.src(paths.scripts.in)
+  return src(paths.scripts.in)
     .pipe(concat(paths.scripts.output_name + '.js'))
-    .pipe(gulp.dest(paths.scripts.out))
+    .pipe(dest(paths.scripts.out))
     .pipe(gulpIf(isProduction, uglify({
       compress: {
         drop_console: true,
@@ -42,13 +24,13 @@ function themeScript() {
       }
     })))
     .pipe(gulpIf(isProduction, rename({ suffix: '.min' })))
-    .pipe(gulp.dest(paths.scripts.out));
+    .pipe(dest(paths.scripts.out));
 }
 
 function vendorScript() {
-  return gulp.src(paths.scripts.vendor.in)
+  return src(paths.scripts.vendor.in)
     .pipe(concat(paths.scripts.vendor.output_name + '.js'))
-    .pipe(gulp.dest(paths.scripts.vendor.out))
+    .pipe(dest(paths.scripts.vendor.out))
     .pipe(uglify({
       compress: {
         drop_console: true,
@@ -56,29 +38,29 @@ function vendorScript() {
       }
     }))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(paths.scripts.vendor.out));
+    .pipe(dest(paths.scripts.vendor.out));
 }
 
 function copyAssets(done) {
   // Misc. root level files
-  gulp.src(paths.copy.root)
-      .pipe(gulp.dest(paths.dist));
+  src(paths.copy.root, {encoding: false})
+      .pipe(dest(paths.dist));
 
   // Images
-  gulp.src(paths.copy.images.in)
-      .pipe(gulp.dest(paths.copy.images.out));
+  src(paths.copy.images.in, {encoding: false})
+      .pipe(dest(paths.copy.images.out));
 
   // Files
-  gulp.src(paths.copy.files.in)
-      .pipe(gulp.dest(paths.copy.files.out));
+  src(paths.copy.files.in, {encoding: false})
+      .pipe(dest(paths.copy.files.out));
 
   // Fonts
-  // gulp.src(paths.copy.fonts.in)
-  //     .pipe(gulp.dest(paths.copy.fonts.out));
+  // src(paths.copy.fonts.in, {encoding: false})
+  //     .pipe(dest(paths.copy.fonts.out));
 
   // Project video files
-  // gulp.src(paths.copy.video.in)
-  //     .pipe(gulp.dest(paths.copy.video.out));
+  // src(paths.copy.video.in, {encoding: false})
+  //     .pipe(dest(paths.copy.video.out));
 
   done();
 }
@@ -99,19 +81,16 @@ function bsReload(done) {
 }
 
 function watchFiles() {
-  // watch(paths.styles.watch, parallel(scssStyles));
   watch(paths.scripts.watch, themeScript);
 };
 
 // Defining complex tasks
-// const buildSite = gulp.series(copyAssets, themeScript, vendorScript, scssStyles);
-const buildSite = gulp.series(copyAssets, themeScript, vendorScript);
-const watchProject = gulp.series(buildSite, gulp.parallel(watchFiles, browserSync));
+const buildSite = series(copyAssets, themeScript, vendorScript);
+const watchProject = series(buildSite, parallel(watchFiles, browserSync));
 
 
 // export tasks
 exports.clean = clean;
-exports.styles = scssStyles;
 exports.build = buildSite;
 exports.watch = watchProject;
 exports.default = buildSite;
