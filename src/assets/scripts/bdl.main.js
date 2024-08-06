c17166d6e8a869919b4e8dcaf58f54fbe0b9a39f
boudreauLabs <boudreaulabs@gmail.com>
2024-08-06 17:03:50 -0400

    Cloudflare turnstile support in forms

--- src/assets/scripts/bdl.main.js
+++ src/assets/scripts/bdl.main.js
@@ -1,24 +1,30 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log('ready');
 
+
   // Forms
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.needs-validation')
+  // https://getbootstrap.com/docs/5.3/forms/validation/
+  const forms = document.querySelectorAll('.needs-validation');
+  let tsValid = false;
+  window.turnstileCallback = function () { tsValid = true; };
+  window.turnstileErrorCallback = function () { tsValid = false; };
   if (forms.length) {
     // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
       form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
+        if (form.checkValidity() == false || tsValid == false) {
           event.preventDefault()
           event.stopPropagation()
         }
-
         form.classList.add('was-validated')
       }, false)
     })
   }
 
   // ScrollMagic
+  // https://scrollmagic.io/docs/
 	const scrollController = new ScrollMagic.Controller();
   const scrlFadeEls = document.getElementsByClassName('js-fadein');
   const progressEls = document.getElementsByClassName('js-progress');
@@ -50,5 +56,36 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
   // ScrollMagic
+
+  // Swiper
+  // https://swiperjs.com/swiper-api
+  const projectCarousells = document.getElementsByClassName('swiper');
+  if (projectCarousells.length) {
+    let slideOpts = {
+      loop: false,
+      autoplay: {
+        delay: 5000,
+      },
+      slidesPerView: 1,
+      spaceBetween: 12,
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true
+      },
+      pagination: {
+        clickable: true,
+        el: ".swiper-pagination",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      speed: 1000,
+    };
+    for (const slider of projectCarousells) {
+      new Swiper(slider, slideOpts);
+    }
+  }
+  // Swiper
 
 });
